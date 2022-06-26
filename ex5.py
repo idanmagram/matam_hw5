b04634e0db598d00ffc18f3af8a833b715dc048f
Idan Magram <idan.magram@gmail.com>
2022-06-26 09:03:57 +0300

    3 tests work

--- ex5.py
+++ ex5.py
@@ -12,7 +12,16 @@ def names_of_registered_students(input_json_path, course_name):
     :param course_name: The name of the course.
     :return: List of the names of the students.
     """
-    pass
+    students_registered_to_course = []
+
+    with open(input_json_path, 'r') as file:
+        db = json.load(file)
+
+    for id in db:
+        if course_name in db[id]['registered_courses']:
+            students_registered_to_course.append(db[id]['student_name'])
+
+    return students_registered_to_course
 
 
 def enrollment_numbers(input_json_path, output_file_path):
@@ -23,8 +32,26 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param input_json_path: Path of the students database json file.
     :param output_file_path: Path of the output text file.
     """
-    pass
 
+    with open(input_json_path, 'r') as file:
+        db = json.load(file)
+
+    courses_amount = {}
+
+    for id in db:
+        for course in db[id]['registered_courses']:
+            if course in courses_amount.keys():
+                courses_amount[course] += 1
+            else:
+                courses_amount[course] = 1
+
+    course_list = list(courses_amount)
+    course_list.sort()
+
+    with open(output_file_path, 'w') as out_file:
+        for course in course_list:
+            line = "\"" + course + "\" " + str(courses_amount[course]) + "\n"
+            out_file.write(line)
 
 
 def courses_for_lecturers(json_directory_path, output_json_path):
@@ -34,7 +61,31 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     :param json_directory_path: Path of the semsters_data files.
     :param output_json_path: Path of the output json file.
     """
-    pass
+    out_dict = {}
+
+    for file_name in os.listdir(json_directory_path):
+        file_path = os.path.join(json_directory_path, file_name)
+        if file_name[-len('.json'):] == '.json':
+            parse_file(file_path, out_dict)
+
+    with open(output_json_path, 'w') as out_file:
+        json.dump(out_dict, out_file)
+
+
+def parse_file(file_path, out_dict):
+    with open(file_path, 'r') as file:
+        db = json.load(file)
+        for course_id in db:
+            for lecturer in db[course_id]["lecturers"]:
+                add_course_to_lecturer(course_id, db, lecturer, out_dict)
+
+
+def add_course_to_lecturer(course_id, db, lecturer, out_dict):
+    if lecturer in out_dict:
+        if db[course_id]["course_name"] not in out_dict[lecturer]:
+            out_dict[lecturer].append(db[course_id]["course_name"])
+    else:
+        out_dict[lecturer] = [db[course_id]["course_name"]]
 
 
 
